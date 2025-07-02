17bfad75a7bc17169ba565956eb476a03988f5ef
KINGTUT10101 <kingtut10101@gmail.com>
2025-07-02 13:14:50 -0400

    Add functionality to home page search bar

--- react/src/components/HomePage.jsx
+++ react/src/components/HomePage.jsx
@@ -3,23 +3,44 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomePage = () => {
-    const url = 'http://localhost:9001/api/characters';
-    const [characters, setCharacters] = useState([]);
+    const url = 'http://localhost:9001/api/characters'
+    const [characters, setCharacters] = useState([])
+    let [fullCharacters, setFullCharacters] = useState(characters)
 
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
-            .then(data => setCharacters(data))
+            .then(data => {
+                setCharacters(data)
+                setFullCharacters (data)
+            })
             .catch(err => console.error(err));
     }, []);
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const searchStr = event.currentTarget.elements.searchStr.value
+
+        const re = new RegExp(searchStr, "i")
+        setCharacters(characters.filter(character => {
+            return re.test(character.name)
+        }))
+    }
+
+    function handleInputChange(event) {
+        if (event.target.value === '') {
+            setCharacters (fullCharacters)
+        }
+    }
 
     return (
         <>
             <h1>Star Wars Universe Lookup</h1>
             <h2>Which character are you looking for?</h2>
-            <form className="d-flex" role="search">
+            <form className="d-flex" role="search" onSubmit={handleSubmit}>
                 <input className="form-control me-2" type="search"
-                    placeholder="Name here" aria-label="Search" />
+                    placeholder="Name here" aria-label="Search" id="searchStr" onChange={handleInputChange} />
                 <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
             <hr />
